f29f79587bc99434381865f9906257344cb991bb
Katsikas <kpete703@gmail.com>
2023-02-02 20:07:04 +0200

    my first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <>
+    <Header></Header>
+    <h2>first React app</h2>
+  </>
+  );
+}
+
+export default App;
